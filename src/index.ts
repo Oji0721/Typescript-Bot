3acda2e1b6bd55e50e7babd7256eec6214639e4b
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-07-25 14:35:08 +0900

    Fix a typo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import("dotenv/config");
 import { ExtendedClient } from "./structures/Client";
-import { Database } from './structures/types';
+import { Database } from './structures/Database';
 
 export const client = new ExtendedClient();
 export const db = new Database();
